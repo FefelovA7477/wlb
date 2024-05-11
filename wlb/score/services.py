592b45de004590f32ee5c0c62bd17c7f531fbab1
Fefelov Aleksandr <fefelov7477@gmail.com>
2024-05-12 02:33:37 +0300

    fix: update and create score both with post request

--- wlb/score/services.py
+++ wlb/score/services.py
@@ -2,6 +2,12 @@ from backend.services import cmn_services
 from .models import Score
 from category import services as category_services
 
+manager = Score.objects
+
+def get_score_instance(**kwargs) -> Score:
+    return cmn_services.get_object(manager, **kwargs)
+
+
 def get_all_user_scores(user: object, **kwargs):
     return filter_scores(
         category__in=category_services.filter_categories(user=user)
@@ -10,19 +16,19 @@ def get_all_user_scores(user: object, **kwargs):
 
 def create_score_object(**kwargs) -> Score:
     return cmn_services.create_object(
-        Score.objects,
+        manager,
         **kwargs
     )
 
 
 def filter_scores(*args, **kwargs):
     return cmn_services.filter_objects(
-        Score.objects,
+        manager,
         **kwargs
     )
 
 
 def get_score_object(**kwargs) -> Score:
-    return cmn_services.get_object(Score.objects, **kwargs)
+    return cmn_services.get_object(manager, **kwargs)
 
 
--- wlb/score/views.py
+++ wlb/score/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, mixins
+from django.utils import timezone
 
 from .serializers import ScoreSerializer
 from .permissions import IsCategoryOwner
@@ -14,5 +15,19 @@ class ScoreViewset(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = score_services.get_all_user_scores(user=self.request.user).order_by('-id')
         return queryset
+    
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except:
+            return super().update(request, *args, **kwargs)
+    
+    def get_object(self):
+        category = self.request.data['category']
+        if 'date' in self.request.data.keys():
+            date = self.request.data['date']
+        else:
+            date = timezone.now().date()
+        return self.get_queryset().filter(category=category, date=date).first()
 
 # Create your views here.
