e464397b9440b7b2b8f4e51cb0d32580cd8b181e
Egor <egor@2949429-cp81016.twc1.net>
2024-05-16 16:34:28 +0300

    fix: all assessments completed functional added

--- wlb/stats/admin.py
+++ wlb/stats/admin.py
@@ -7,7 +7,7 @@ class MetricAdmin(admin.ModelAdmin):
     list_display = ('user', 'get_user_login_time', 'clicked_get_started_first_screen',
                     'clicked_view_example_second_screen', 'clicked_next_second_screen',
                     'clicked_add_own_category_third_screen', 'clicked_next_with_categories_third_screen',
-                    'clicked_time_selection_onboarding', 'completed_all_assessments')
+                    'clicked_time_selection_onboarding', 'is_completed_all_assessments')
     
     @admin.display(description='Дата создания')
     def get_user_login_time(self, obj):
@@ -15,6 +15,14 @@ class MetricAdmin(admin.ModelAdmin):
             return obj.user.date_joined
         except:
             return None
+
+    @admin.display(description='Прошел оценку всех параметров')
+    def is_completed_all_assessments(self, obj):
+        try:
+            return obj.is_completed_all_assessments()
+        except:
+            return False
+    is_completed_all_assessments.boolean = True
 
 admin.site.register(Metric, MetricAdmin)
 
--- wlb/stats/models.py
+++ wlb/stats/models.py
@@ -19,9 +19,16 @@ class Metric(models.Model):
                                                                     verbose_name="Нажал 'далее' на третьем экране с категориями")
     clicked_time_selection_onboarding = models.BooleanField(default=False, 
                                                             verbose_name="Нажал на выбор времени в онбординге")
-    completed_all_assessments = models.BooleanField(default=False, verbose_name="Прошел оценку всех параметров")
 
     class Meta:
         verbose_name = "Статистика онбординга"
         verbose_name_plural = "Статистики онбордингов"
+
+    def is_completed_all_assessments(self):
+        return self.clicked_get_started_first_screen \
+                and self.clicked_view_example_second_screen \
+                and self.clicked_next_second_screen \
+                and self.clicked_add_own_category_third_screen \
+                and self.clicked_next_with_categories_third_screen \
+                and self.clicked_time_selection_onboarding
 # Create your models here.
